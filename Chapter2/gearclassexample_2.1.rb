b681b516d8d95dbe948cebca7ee9de9dc40616ca
pkelliher <pkelliher74@gmail.com>
2021-03-14 17:59:25 -0700

    gearExample2.1 and 2.2

---
+++ Chapter2/gearclassexample_2.1.rb
@@ -0,0 +1,10 @@
+// number of teeth
+chainring =  52
+cog = 11
+ratio = chaingring / cog.to_f
+puts ratio # => 4.72727272
+
+chainring = 30
+cog = 27
+ratio = chainring / cog.to_f
+puts ratio # => 1.11111111
