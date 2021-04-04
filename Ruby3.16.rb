83a22c81b3b2720ca54eec11672e344d49ebfc66
pkelliher <pkelliher74@gmail.com>
2021-04-03 17:59:06 -0700

    3.16 middle

--- Ruby3.16.rb
+++ Ruby3.16.rb
@@ -15,5 +15,25 @@ class Gear
 end
 
 class Wheel
-    attr_reader :
+    attr_reader :rim, :tire, :Gear
+    def initialize(rim:, tire:, chainring:, cog:)
+        @rim = rim
+        @tire = tire
+        @gear = Gear.new(chainring: chainring, cog: cog)
+    end
 
+    def diameter
+        rim + (tire * 2)
+    end
+
+    def gear_inches
+        gear.gear_inches(diameter)
+    end
+end
+
+puts Wheel.new(
+    rim: 26,
+    tire: 1.5,
+    chainring: 52,
+    cog: 11).gear_inches
+# => 137.0909090909
