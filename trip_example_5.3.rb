c72fb80264a41027e2037e243baf09f632f9df50
pkelliher <pkelliher74@gmail.com>
2021-04-07 19:34:22 -0700

    update to trip example

--- trip_example_5.3.rb
+++ trip_example_5.3.rb
@@ -17,3 +17,16 @@ class Mechanic
     end
 end
 
+class TripCoordinator
+    def prepare_trip(trip)
+        buy_food(trip.customers)
+    end
+end
+
+class Driver
+    def prepare_trip(trip)
+        vehicle = trip.vehicle
+        gas_up(vehicle)
+        fill_water_tank(vehicle)
+    end
+end
