197771a15c6cce31847d97f244e80ef14cef5a05
pkelliher <pkelliher74@gmail.com>
2021-04-11 19:01:12 -0700

    Chapter8

---
+++ Chapter8/Bicycle.rb
@@ -0,0 +1,54 @@
+class Bicycle
+    attr_reader :size, :parts
+
+    def initialize(size:, parts:)
+        @size   = size
+        @parts  = parts
+    end
+
+    def spares
+        parts.spares
+    end
+end
+
+require 'forwardable'
+class Parts
+    extend Forwardable
+    def_delegators :@parts, :size, :each
+    include Enumerable
+
+    def initialize(parts)
+        @parts = parts
+    end
+
+    def spares
+        select {|part| part.need_spare}
+    end
+end
+
+require 'ostruct'
+module PartsFactory
+    def self.build(config:, parts_class: Parts)
+        parts_class.new(
+            config.collect {|part_config|
+            create_part(part_config)})
+    end
+
+    def self.create_part(part_config)
+        OpenStruct.new(
+            name: part_config[0],
+            description: part_config[1],
+            need_spare: part_config.fetch(2, true))
+    end
+end
+
+road_config = 
+[['chain',  '11-speed'],
+['tire_size',   '23'],
+['tape_color',  'red']]
+
+mountain_config = 
+[['chain',  '11-speed'],
+['tire_size',   '2.1'],
+['front_shock', 'Manitou'],
+['rear_shock',  'Fox', false]]
